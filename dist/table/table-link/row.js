5ead9794c45fde4ca5cd0be77bc6d1d62eb69ab2
ciaolee87 <ciaolee87@gmail.com>
2025-04-18 11:29:55 +0900

    0.1.29

--- dist/table/table-link/row.js
+++ dist/table/table-link/row.js
@@ -3,17 +3,18 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import { useRouter } from "next/navigation";
 export default function ({ children, href }) {
     const router = useRouter();
-    return (_jsx("tr", { onClick: (e) => {
-            e.stopPropagation();
-            e.preventDefault();
-            switch (e.button) {
-                case 0:
-                    router.push(href);
-                    return;
-                case 1:
-                    window.open(href, "_blank");
-                    return;
-            }
-        }, children: children }));
+    const onClick = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        switch (e.button) {
+            case 0:
+                router.push(href);
+                return;
+            case 1:
+                window.open(href, "_blank");
+                return;
+        }
+    };
+    return (_jsx("tr", { onClick: onClick, onMouseDown: onClick, children: children }));
 }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm93LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3RhYmxlL3RhYmxlLWxpbmsvcm93LnRzeCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxZQUFZLENBQUM7O0FBQ2IsT0FBTyxFQUFDLFNBQVMsRUFBQyxNQUFNLGlCQUFpQixDQUFDO0FBUTFDLE1BQU0sQ0FBQyxPQUFPLFdBQVcsRUFBQyxRQUFRLEVBQUUsSUFBSSxFQUFrQjtJQUN6RCxNQUFNLE1BQU0sR0FBRyxTQUFTLEVBQUUsQ0FBQztJQUMzQixPQUFPLENBQ04sYUFDQyxPQUFPLEVBQUUsQ0FBQyxDQUFDLEVBQUUsRUFBRTtZQUNkLENBQUMsQ0FBQyxlQUFlLEVBQUUsQ0FBQztZQUNwQixDQUFDLENBQUMsY0FBYyxFQUFFLENBQUM7WUFDbkIsUUFBUSxDQUFDLENBQUMsTUFBTSxFQUFFLENBQUM7Z0JBQ2xCLEtBQUssQ0FBQztvQkFFTCxNQUFNLENBQUMsSUFBSSxDQUFDLElBQUksQ0FBQyxDQUFDO29CQUNsQixPQUFPO2dCQUNSLEtBQUssQ0FBQztvQkFFTCxNQUFNLENBQUMsSUFBSSxDQUFDLElBQUksRUFBRSxRQUFRLENBQUMsQ0FBQztvQkFDNUIsT0FBTztZQUNULENBQUM7UUFDRixDQUFDLFlBQ0EsUUFBUSxHQUNMLENBQ0wsQ0FBQztBQUNILENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicm93LmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL3RhYmxlL3RhYmxlLWxpbmsvcm93LnRzeCJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxZQUFZLENBQUM7O0FBQ2IsT0FBTyxFQUFDLFNBQVMsRUFBQyxNQUFNLGlCQUFpQixDQUFDO0FBUTFDLE1BQU0sQ0FBQyxPQUFPLFdBQVcsRUFBQyxRQUFRLEVBQUUsSUFBSSxFQUFrQjtJQUN6RCxNQUFNLE1BQU0sR0FBRyxTQUFTLEVBQUUsQ0FBQztJQUMzQixNQUFNLE9BQU8sR0FBaUQsQ0FBQyxDQUFDLEVBQUUsRUFBRTtRQUNuRSxDQUFDLENBQUMsZUFBZSxFQUFFLENBQUM7UUFDcEIsQ0FBQyxDQUFDLGNBQWMsRUFBRSxDQUFDO1FBQ25CLFFBQVEsQ0FBQyxDQUFDLE1BQU0sRUFBRSxDQUFDO1lBQ2xCLEtBQUssQ0FBQztnQkFFTCxNQUFNLENBQUMsSUFBSSxDQUFDLElBQUksQ0FBQyxDQUFDO2dCQUNsQixPQUFPO1lBQ1IsS0FBSyxDQUFDO2dCQUVMLE1BQU0sQ0FBQyxJQUFJLENBQUMsSUFBSSxFQUFFLFFBQVEsQ0FBQyxDQUFDO2dCQUM1QixPQUFPO1FBQ1QsQ0FBQztJQUNGLENBQUMsQ0FBQztJQUNGLE9BQU8sQ0FDTixhQUNDLE9BQU8sRUFBRSxPQUFPLEVBQ2hCLFdBQVcsRUFBRSxPQUFPLFlBQ25CLFFBQVEsR0FDTCxDQUNMLENBQUM7QUFDSCxDQUFDIn0=
--- src/table/table-link/row.tsx
+++ src/table/table-link/row.tsx
@@ -9,22 +9,24 @@ interface Props {
 
 export default function ({children, href}: Readonly<Props>) {
 	const router = useRouter();
+	const onClick: React.MouseEventHandler<HTMLTableRowElement> = (e) => {
+		e.stopPropagation();
+		e.preventDefault();
+		switch (e.button) {
+			case 0:
+				// e.button = 0 : left click
+				router.push(href);
+				return;
+			case 1:
+				// e.button = 1 : center click
+				window.open(href, "_blank");
+				return;
+		}
+	};
 	return (
 		<tr
-			onClick={(e) => {
-				e.stopPropagation();
-				e.preventDefault();
-				switch (e.button) {
-					case 0:
-						// e.button = 0 : left click
-						router.push(href);
-						return;
-					case 1:
-						// e.button = 1 : center click
-						window.open(href, "_blank");
-						return;
-				}
-			}}>
+			onClick={onClick}
+			onMouseDown={onClick}>
 			{children}
 		</tr>
 	);
