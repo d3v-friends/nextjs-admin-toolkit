a24211e68a680939aa1c9ce90ddd2bc4180f207c
ciaolee87 <ciaolee87@gmail.com>
2025-04-18 11:20:05 +0900

    0.1.27

--- src/layout/section-3/index.tsx
+++ src/layout/section-3/index.tsx
@@ -1,6 +1,6 @@
+import {fnCss} from "nextjs-tools";
 import {ReactNode} from "react";
 import Widget, {Props as WidgetProps} from "./widget";
-import {fnCss} from "nextjs-tools";
 
 interface Props extends WidgetProps {
 	children?: ReactNode;
@@ -10,7 +10,7 @@ export default function ({children, header, asideMenus, top}: Readonly<Props>) {
 	return (
 		<div className={"flex flex-col h-full min-h-screen relative"}>
 			<Widget {...{header, asideMenus, top}} />
-			<main className={fnCss.sum("flex-1 lg:ml-(--width-aside) min-h-screen transition-all duration-300")}>
+			<main className={fnCss.sum("flex-1 lg:ml-(--width-aside) transition-all duration-300")}>
 				<div className="p-3 lg:p-5">{children}</div>
 			</main>
 		</div>
